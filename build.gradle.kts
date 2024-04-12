0c981d6b50dd517a2c661f060c93691d1e2cd2b4
Bernhard Kern <bernhard.kern@iits-consulting.de>
2024-04-12 16:47:50 +0200

    test work with spring-data-elasticsearch-m2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.2.2"
+    id("org.springframework.boot") version "3.2.4"
     id("io.spring.dependency-management") version "1.1.4"
-    kotlin("jvm") version "1.9.22"
-    kotlin("plugin.spring") version "1.9.22"
+    kotlin("jvm") version "1.9.23"
+    kotlin("plugin.spring") version "1.9.23"
 }
 
 group = "kern"
@@ -14,21 +14,22 @@ java {
     sourceCompatibility = JavaVersion.VERSION_21
 }
 
-val testcontainersVersion = "1.19.3"
+val testcontainersVersion = "1.19.7"
 
 dependencyManagement {
 	imports {
-		mavenBom("org.testcontainers:testcontainers-bom:$testcontainersVersion")
+        mavenBom("org.testcontainers:testcontainers-bom:$testcontainersVersion")
 	}
 }
 
 repositories {
+    maven { url = uri("https://repo.spring.io/milestone") } //spring-data-elasticsearch-m2
     mavenCentral()
 }
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-elasticsearch")
-	implementation("org.springframework.data:spring-data-elasticsearch")
+    implementation("org.springframework.data:spring-data-elasticsearch:5.3.0-M2")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
--- src/main/kotlin/kern/elasticsearchplayground/NativeQueryWithCriteriaQueryWithLocationBug.kt
+++ src/main/kotlin/kern/elasticsearchplayground/NativeQueryWithCriteriaQueryWithLocationBug.kt
@@ -38,15 +38,16 @@ class WithGeoLocationService(val repository: WithGeoLocationRepository, val oper
     }
 
     fun searchOnlyWithNativeQuery(): SearchHits<WithGeoLocation> {
-        val nativeGeoDistance = GeoDistanceQuery.Builder().field("location").distance("10km")
-            .location(GeoLocation.Builder().latlon(LatLonGeoLocation.Builder().lat(pointWeAreLookingFor.lat).lon(pointWeAreLookingFor.lon).build()).build()).build()
+        val nativeGeoDistance = GeoDistanceQuery.Builder().field("location").distance("1km")
+            .location(GeoLocation.Builder().latlon(LatLonGeoLocation.Builder().lat(pointWeAreLookingFor.lat).lon(pointWeAreLookingFor.lon).build()).build())
+            .build()
         val nq = NativeQueryBuilder().withQuery { q -> q.geoDistance(nativeGeoDistance) }.build()
 
         return operations.search(nq, WithGeoLocation::class.java)
     }
 
     fun searchWithNativeQueryMixedWithCriteriaQueryAndGeoLocation(): SearchHits<WithGeoLocation> {
-        val cq = CriteriaQuery(Criteria("location").within(pointWeAreLookingFor, "10km"))
+        val cq = CriteriaQuery(Criteria("location").within(pointWeAreLookingFor, "1km"))
         val nq = NativeQueryBuilder().withQuery(cq).build()
 
         return operations.search(nq, WithGeoLocation::class.java)
